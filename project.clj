007f6d44253fa84c0de01e3e3b538605adb3bc12
Mitchell Thomas <mthomas@tripwire.com>
2015-01-09 12:28:26 -0800

    Add activemq code an deps as well as protobuf deps

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject message-bus "0.1.0-SNAPSHOT"
   :description "An example of using ActiveMQ and Protobuf with Clojure."
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.apache.activemq/apache-activemq "5.10.0"]
+                 [org.flatland/protobuf "0.8.1"]]
   :main ^:skip-aot message-bus.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/message_bus/core.clj
+++ src/message_bus/core.clj
@@ -1,7 +1,52 @@
 (ns message-bus.core
+  (:require
+   [clojure.core.async :refer [go timeout put! <! chan >!]])
+  (:import
+   [org.apache.activemq ActiveMQConnectionFactory ActiveMQTopic])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(comment
+  (defn start-activemq-session!
+    [broker & {username :username password :password max-connections :max-connections :or {max-connections 1}}]
+    "Returns an ActiveMQ connection which has been started.
+     It currently supports the following optional named
+     arguments (refer to ActiveMQ docs for more details about them):
+     :username, :password"
+    (when (nil? broker) (throw (IllegalArgumentException. "No value specified for broker URL!")))
+    (let [connection (doto (ActiveMQConnectionFactory. broker) (.createConnection username password))
+          _ (.start connection)
+          session (.createSession connection false Session/AUTO_ACKNOWLEDGE)]
+      {:connection  connection
+       :session session}))
+
+  (defn stop-activemq-session!
+    [session]
+    (.close :session session)
+    (.close (:connection session))
+    true)
+
+  (defn create-activemq-chan
+    [session topic-name]
+    (let [ch (chan)
+          s (:session session)
+          ml (reify java.jms.MessageListener
+               (onMessage [this mess]
+                 (let [cl (class mess)]
+                   (cond
+                     (instance? javax.jmx.BytesMessage cl) (let [ba (byte-array (.getBodyLength mess))
+                                                                 _ (.readBytes mess ba)]
+                                                             (>! ch ba))
+                     (instance? javax.jmx.TextMessage cl) (let [tx (.getText mess)]
+                                                            (>! ch tx)))
+                   (.acknowledge mess)
+                   (if (.isClosed s) (close! ch)))))
+          topic (ActiveMQTopic. topic-name)
+          consumer (.createConsumer s topic ml)]
+      ch))
+
+  )
