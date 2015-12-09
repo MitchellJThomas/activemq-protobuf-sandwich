d37691328cb5a98e4ba793f6e957cb124dcbb3fe
Mitchell Thomas <mthomas@tripwire.com>
2015-12-09 13:30:42 -0800

    Freshen deps

--- project.clj
+++ project.clj
@@ -4,15 +4,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "An example of using Appche ActiveMQ and Google Protobuf with Clojure."
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.apache.activemq/activemq-client "5.12.1"]
+                 [org.clojure/core.async "0.2.374"]
+                 [org.apache.activemq/activemq-client "5.13.0"]
                  [org.flatland/protobuf "0.8.1"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.slf4j/slf4j-log4j12 "1.7.12"]]
-  :plugins [[lein-protobuf "0.4.1"]
-            [cider/cider-nrepl "0.10.0-SNAPSHOT"]]
+                 [org.slf4j/slf4j-log4j12 "1.7.13"]]
+  :plugins [[lein-protobuf "0.4.1"]]
   :main ^:skip-aot message-bus.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[org.apache.activemq/activemq-broker "5.12.1"]]}}
+             :dev {:dependencies [[org.apache.activemq/activemq-broker "5.13.0"]]}}
   )
